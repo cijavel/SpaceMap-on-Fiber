e21c594ef4773f693035831f752ffa049738a31d
Cijavel <Konto--github@l7t.de>
2025-11-15 04:15:56 +0100

    feat: add more spaces

--- src/DataSpaceList.cpp
+++ src/DataSpaceList.cpp
@@ -5,11 +5,32 @@
 // --------------------------------------------------------------------------
 // LED No, Name for parsing
 SpaceSearchList searchList[] = {
-    { 0, "OpenLab Augsburg"                   }, 
-    { 1, "IT-Syndikat"                        }, 
-    { 2, "MuCCC"                              }, 
-    { 3, "realraum"                           }, 
-    { 4, "Binary Kitchen"                     }
+    { 0, "OpenLab Augsburg"                  , "Augsburg"},
+    { 1, "IT-Syndikat"                       , "Innsbruck"}, 
+    { 2, "MuCCC"                             , "Munich"},
+    { 3, "realraum"                          , "Graz"},
+    { 4, "Binary Kitchen"                    , "Regensburg"},
+    { 5, "c-base"                            , "Berlin"},
+    { 6, "Chaosdorf"                         , "Düsseldorf"},
+    { 7, "CCC Frankfurt"                     , "Frankfurt"},
+    { 8, "backspace"                         , "Bamberg"},
+    { 9, "CCCHH"                             , "Hamburg" },
+    {10, "vspace.one"                        , "VS-Villingen"},
+    {11, "Nerdberg"                          , "Fuerth"},
+    {12, "dezentrale"                        , "Leipzig"},
+   //{13, "shackspace - stuttgart hackerspace", "Stuttgart"},
+   //{14, "temporaerhaus"                     , "Ulm"},
+   //{15, "Chaostreff Bern"                   , "Bern"},
+   //{16, "hacKNology e.V."                   , "Konstanz"},
+   //{17, "CCCFr"                             , "Freiburg im Breisgau"},
+   //{18, "turmlabor"                         , "Dresden"},
+   //{19, "Eigenbaukombinat Halle e.V."       , "Halle (Saale)"},
+   //{20, "Krautspace – Hackspace Jena e.V."  , "Jena"},
+   //{21, "flipdot"                           , "Kassel"},
+   //{22, "CCC Frankfurt"                     , "Frankfurt"},
+   //{23, "Chaostreff Dortmund"               , "Dortmund"},
+   //{24, "Hackerspace Bremen e.V."           , "Bremen"},
+   //{25, "HSBXL"                             , "Brussels"},
 };
 
 // --------------------------------------------------------------------------
